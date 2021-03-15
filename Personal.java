c1631654e84d05de38f371084f088d3efa081ebe
deblur99 <deblur99@kakao.com>
2021-03-15 19:14:38 +0900

    .

--- Personal.java
+++ Personal.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Personal {
-    import java.util.Scanner;
     public static void main(String[] args) {
-        Scanner scanner = 
+        // inputs : name, country, age, weight, single
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        String country = scanner.next();
+        int age = scanner.nextInt();
+        double weight = scanner.nextDouble();
+        boolean single = scanner.nextBoolean();
+
+        System.out.println(name+" is from "+country+", "+age+" years old, "+weight+" kg.");
+        System.out.println(name+" is "+(single ? "Single" : "Married"));
+        scanner.close();
     }
 }
